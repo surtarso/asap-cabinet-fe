4061a73ceaed491488701bc9838d63a7d1babbcb
surtarso <tarso.galvao@gmail.com>
2025-03-01 15:09:16 -0300

    removed failed attempts of modularizations since the single script is now more up to date.

--- buggy/src/main_window/__init__.py
+++
@@ -1,2 +0,0 @@
-# __init__.py
-from .single_table_viewer import SingleTableViewer
--- buggy2/__init__.py
+++
@@ -1,3 +0,0 @@
-# asap_cabinet_fe/__init__.py
-# This file can be empty or used for package initialization
-__version__ = "0.1.0"
