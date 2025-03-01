09a670d4992ba34fc1318dba42d0500c8794e3be
surtarso <tarso.galvao@gmail.com>
2025-02-28 22:08:01 -0300

    organized configs in sections

--- src/config.py
+++ src/config.py
@@ -1,13 +1,12 @@
 import os
 import configparser
-
 '''
 This module holds all configuration constants 
 and the function to load settings from the INI file.
 '''
 
 # Path to the configuration file
-CONFIG_FILE = "~/.asap-cabinet-fe/settings.ini"
+CONFIG_FILE             = "~/.asap-cabinet-fe/settings.ini"
 
 # Main paths and commands
 VPX_ROOT_FOLDER         = "/home/tarso/Games/vpinball/build/tables/"
@@ -56,12 +55,11 @@ def load_configuration():
     Updates the module-level variables.
     """
     global VPX_ROOT_FOLDER, EXECUTABLE_CMD, EXECUTABLE_SUB_CMD
+    global DEFAULT_TABLE_PATH, DEFAULT_WHEEL_PATH, DEFAULT_BACKGLASS_PATH, DEFAULT_DMD_PATH
     global TABLE_IMAGE_PATH, TABLE_WHEEL_PATH, TABLE_BACKGLASS_PATH, TABLE_DMD_PATH
     global MAIN_MONITOR_INDEX, MAIN_WINDOW_WIDTH, MAIN_WINDOW_HEIGHT
     global SECONDARY_MONITOR_INDEX, BACKGLASS_WINDOW_WIDTH, BACKGLASS_WINDOW_HEIGHT
-
     global BACKGLASS_IMAGE_WIDTH, BACKGLASS_IMAGE_HEIGHT, DMD_WIDTH, DMD_HEIGHT
-
     global WHEEL_IMAGE_SIZE, WHEEL_IMAGE_MARGIN, FONT_NAME, FONT_SIZE
     global BG_COLOR, TEXT_COLOR, FADE_DURATION, FADE_OPACITY
 
@@ -74,16 +72,24 @@ def load_configuration():
     if os.path.exists(ini_file):
         config.read(ini_file)
     else:
-        config['Settings'] = {
+        config['Main Paths'] = {
             "VPX_ROOT_FOLDER":         VPX_ROOT_FOLDER,
             "EXECUTABLE_CMD":          EXECUTABLE_CMD,
             "EXECUTABLE_SUB_CMD":      EXECUTABLE_SUB_CMD,
-
+        }
+        config['Default Images'] = {
+            "DEFAULT_TABLE_PATH":      DEFAULT_TABLE_PATH,
+            "DEFAULT_WHEEL_PATH":      DEFAULT_WHEEL_PATH,
+            "DEFAULT_BACKGLASS_PATH":  DEFAULT_BACKGLASS_PATH,
+            "DEFAULT_DMD_PATH":        DEFAULT_DMD_PATH,
+        }
+        config['Custom Images'] = {
             "TABLE_IMAGE_PATH":        TABLE_IMAGE_PATH,
             "TABLE_WHEEL_PATH":        TABLE_WHEEL_PATH,
             "TABLE_BACKGLASS_PATH":    TABLE_BACKGLASS_PATH,
             "TABLE_DMD_PATH":          TABLE_DMD_PATH,
-
+        }
+        config['Main Window'] = {
             "MAIN_MONITOR_INDEX":      str(MAIN_MONITOR_INDEX),
             "MAIN_WINDOW_WIDTH":       str(MAIN_WINDOW_WIDTH),
             "MAIN_WINDOW_HEIGHT":      str(MAIN_WINDOW_HEIGHT),
@@ -93,7 +99,8 @@ def load_configuration():
             "FONT_SIZE":               str(FONT_SIZE),
             "BG_COLOR":                BG_COLOR,
             "TEXT_COLOR":              TEXT_COLOR,
-
+        }
+        config['Secondary Window'] = {
             "SECONDARY_MONITOR_INDEX": str(SECONDARY_MONITOR_INDEX),
             "BACKGLASS_WINDOW_WIDTH":  str(BACKGLASS_WINDOW_WIDTH),
             "BACKGLASS_WINDOW_HEIGHT": str(BACKGLASS_WINDOW_HEIGHT),
@@ -101,41 +108,54 @@ def load_configuration():
             "BACKGLASS_IMAGE_HEIGHT":  str(BACKGLASS_IMAGE_HEIGHT),
             "DMD_WIDTH":               str(DMD_WIDTH),
             "DMD_HEIGHT":              str(DMD_HEIGHT),
-
+        }
+        config['Transitions'] = {
             "FADE_DURATION":           str(FADE_DURATION),
             "FADE_OPACITY":            str(FADE_OPACITY),
         }
 
         with open(ini_file, "w") as f:
             config.write(f)
-    s = config['Settings']
-    VPX_ROOT_FOLDER         = s.get("VPX_ROOT_FOLDER", VPX_ROOT_FOLDER)
-    EXECUTABLE_CMD          = s.get("EXECUTABLE_CMD", EXECUTABLE_CMD)
-    EXECUTABLE_SUB_CMD      = s.get("EXECUTABLE_SUB_CMD", EXECUTABLE_SUB_CMD)
 
-    TABLE_IMAGE_PATH        = s.get("TABLE_IMAGE_PATH", TABLE_IMAGE_PATH)
-    TABLE_WHEEL_PATH        = s.get("TABLE_WHEEL_PATH", TABLE_WHEEL_PATH)
-    TABLE_BACKGLASS_PATH    = s.get("TABLE_BACKGLASS_PATH", TABLE_BACKGLASS_PATH)
-    TABLE_DMD_PATH          = s.get("TABLE_DMD_PATH", TABLE_DMD_PATH)
+    # Load settings from the appropriate sections
+    p = config['Main Paths']
+    VPX_ROOT_FOLDER         = p.get("VPX_ROOT_FOLDER", VPX_ROOT_FOLDER)
+    EXECUTABLE_CMD          = p.get("EXECUTABLE_CMD", EXECUTABLE_CMD)
+    EXECUTABLE_SUB_CMD      = p.get("EXECUTABLE_SUB_CMD", EXECUTABLE_SUB_CMD)
 
-    MAIN_MONITOR_INDEX      = int(s.get("MAIN_MONITOR_INDEX", MAIN_MONITOR_INDEX))
-    MAIN_WINDOW_WIDTH       = int(s.get("MAIN_WINDOW_WIDTH", MAIN_WINDOW_WIDTH))
-    MAIN_WINDOW_HEIGHT      = int(s.get("MAIN_WINDOW_HEIGHT", MAIN_WINDOW_HEIGHT))
-    WHEEL_IMAGE_SIZE        = int(s.get("WHEEL_IMAGE_SIZE", WHEEL_IMAGE_SIZE))
-    WHEEL_IMAGE_MARGIN      = int(s.get("WHEEL_IMAGE_MARGIN", WHEEL_IMAGE_MARGIN))
-    FONT_NAME               = s.get("FONT_NAME", FONT_NAME)
-    FONT_SIZE               = int(s.get("FONT_SIZE", FONT_SIZE))
-    BG_COLOR                = s.get("BG_COLOR", BG_COLOR)
-    TEXT_COLOR              = s.get("TEXT_COLOR", TEXT_COLOR)
+    di = config['Default Images']
+    DEFAULT_TABLE_PATH      = di.get("DEFAULT_TABLE_PATH", DEFAULT_TABLE_PATH)
+    DEFAULT_WHEEL_PATH      = di.get("DEFAULT_WHEEL_PATH", DEFAULT_WHEEL_PATH)
+    DEFAULT_BACKGLASS_PATH  = di.get("DEFAULT_BACKGLASS_PATH", DEFAULT_BACKGLASS_PATH)
+    DEFAULT_DMD_PATH        = di.get("DEFAULT_DMD_PATH", DEFAULT_DMD_PATH)
 
-    SECONDARY_MONITOR_INDEX = int(s.get("SECONDARY_MONITOR_INDEX", SECONDARY_MONITOR_INDEX))
-    BACKGLASS_WINDOW_WIDTH  = int(s.get("BACKGLASS_WINDOW_WIDTH", BACKGLASS_WINDOW_WIDTH))
-    BACKGLASS_WINDOW_HEIGHT = int(s.get("BACKGLASS_WINDOW_HEIGHT", BACKGLASS_WINDOW_HEIGHT))
-    BACKGLASS_IMAGE_WIDTH   = int(s.get("BACKGLASS_IMAGE_WIDTH", BACKGLASS_IMAGE_WIDTH))
-    BACKGLASS_IMAGE_HEIGHT  = int(s.get("BACKGLASS_IMAGE_HEIGHT", BACKGLASS_IMAGE_HEIGHT))
-    DMD_WIDTH               = int(s.get("DMD_WIDTH", DMD_WIDTH))
-    DMD_HEIGHT              = int(s.get("DMD_HEIGHT", DMD_HEIGHT))
+    pt = config['Custom Images']
+    TABLE_IMAGE_PATH        = pt.get("TABLE_IMAGE_PATH", TABLE_IMAGE_PATH)
+    TABLE_WHEEL_PATH        = pt.get("TABLE_WHEEL_PATH", TABLE_WHEEL_PATH)
+    TABLE_BACKGLASS_PATH    = pt.get("TABLE_BACKGLASS_PATH", TABLE_BACKGLASS_PATH)
+    TABLE_DMD_PATH          = pt.get("TABLE_DMD_PATH", TABLE_DMD_PATH)
 
-    
-    FADE_DURATION           = int(s.get("FADE_DURATION", FADE_DURATION))
-    FADE_OPACITY            = float(s.get("FADE_OPACITY", FADE_OPACITY))
+    mw = config['Main Window']
+    MAIN_MONITOR_INDEX      = int(mw.get("MAIN_MONITOR_INDEX", MAIN_MONITOR_INDEX))
+    MAIN_WINDOW_WIDTH       = int(mw.get("MAIN_WINDOW_WIDTH", MAIN_WINDOW_WIDTH))
+    MAIN_WINDOW_HEIGHT      = int(mw.get("MAIN_WINDOW_HEIGHT", MAIN_WINDOW_HEIGHT))
+    WHEEL_IMAGE_SIZE        = int(mw.get("WHEEL_IMAGE_SIZE", WHEEL_IMAGE_SIZE))
+    WHEEL_IMAGE_MARGIN      = int(mw.get("WHEEL_IMAGE_MARGIN", WHEEL_IMAGE_MARGIN))
+    FONT_NAME               = mw.get("FONT_NAME", FONT_NAME)
+    FONT_SIZE               = int(mw.get("FONT_SIZE", FONT_SIZE))
+    BG_COLOR                = mw.get("BG_COLOR", BG_COLOR)
+    TEXT_COLOR              = mw.get("TEXT_COLOR", TEXT_COLOR)
+
+    sw = config['Secondary Window']
+    SECONDARY_MONITOR_INDEX = int(sw.get("SECONDARY_MONITOR_INDEX", SECONDARY_MONITOR_INDEX))
+    BACKGLASS_WINDOW_WIDTH  = int(sw.get("BACKGLASS_WINDOW_WIDTH", BACKGLASS_WINDOW_WIDTH))
+    BACKGLASS_WINDOW_HEIGHT = int(sw.get("BACKGLASS_WINDOW_HEIGHT", BACKGLASS_WINDOW_HEIGHT))
+    BACKGLASS_IMAGE_WIDTH   = int(sw.get("BACKGLASS_IMAGE_WIDTH", BACKGLASS_IMAGE_WIDTH))
+    BACKGLASS_IMAGE_HEIGHT  = int(sw.get("BACKGLASS_IMAGE_HEIGHT", BACKGLASS_IMAGE_HEIGHT))
+    DMD_WIDTH               = int(sw.get("DMD_WIDTH", DMD_WIDTH))
+    DMD_HEIGHT              = int(sw.get("DMD_HEIGHT", DMD_HEIGHT))
+
+    t = config['Transitions']
+    FADE_DURATION           = int(t.get("FADE_DURATION", FADE_DURATION))
+    FADE_OPACITY            = float(t.get("FADE_OPACITY", FADE_OPACITY))
+
