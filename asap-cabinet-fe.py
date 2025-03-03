ae34ca89fe32c688ea8bb61418e5caf07aa3f0db
surtarso <tarso.galvao@gmail.com>
2025-03-03 13:03:48 -0300

    resolved generic home

--- asap-cabinet-fe.py
+++ asap-cabinet-fe.py
@@ -76,8 +76,8 @@ SND_TABLE_CHANGE        = "snd/table_change.wav"
 # ------------------------------------------
 ##     Included in settins dialog:
 # ------------------------------------------
-VPX_ROOT_FOLDER         = "/home/tarso/Games/vpinball/build/tables/"
-EXECUTABLE_CMD          = "/home/tarso/Games/vpinball/build/VPinballX_GL"
+VPX_ROOT_FOLDER         = "~/Games/vpinball/build/tables/"
+EXECUTABLE_CMD          = "~/Games/vpinball/build/VPinballX_GL"
 EXECUTABLE_SUB_CMD      = "-Play"
 
 # Per table images path (/tables/<table_dir>/)
